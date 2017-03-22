4cbb9260952d8ae693a3ec2b1713c30c69f39b59
oly_aney <olya_aneitchik@yahoo.com>
2017-03-22 15:33:29 +0300

    Fix reduce script

--- InMemoryMapReduce/mapReduce.py
+++ InMemoryMapReduce/mapReduce.py
@@ -9,13 +9,28 @@ if len(sys.argv) > 5:
         script_params.append(sys.argv[i])
 src_file = sys.argv[len(sys.argv) - 2]
 dst_file = sys.argv[len(sys.argv) - 1]
-output = open(dst_file, "w")
-input = open(src_file, "r")
+f_output = open(dst_file, "w")
+f_input = open(src_file, "r")
 if operation == "map":
-    map_proc = Popen(["python", path_to_script] + script_params, stdin = input, stdout = PIPE, stderr = PIPE)
-    input.write(map_proc.communicate()[0])
+    map_proc = Popen(["python", path_to_script] + script_params, stdin = f_input, stdout = PIPE, stderr = PIPE)
+    f_output.write(map_proc.communicate()[0])
+
 elif operation == "reduce":
-    sort_proc = Popen(["python", str(__file__).strip("mapReduce.py") + "sort.py"], stdin = input, stdout = PIPE, stderr = PIPE)
-    reduce_proc = Popen(["python", path_to_script], stdin = sort_proc.stdout, stdout = PIPE, stderr = PIPE)
-    output.write(reduce_proc.communicate()[0])
-output.close()
+    sort_proc = Popen(["python", str(__file__).strip("mapReduce.py") + "sort.py"], stdin = f_input, stdout = PIPE, stderr = PIPE)
+
+    key, val = sort_proc.stdout.readline().split('\t')
+    in_reduce = key + '\t' + val
+
+    for line in sort_proc.stdout:
+        temp_key = line.split('\t')[0]
+        if temp_key == key:
+            in_reduce += temp_key + '\t' + val
+        else:
+            key = temp_key
+            reduce_proc = Popen(["python", path_to_script], stdin = PIPE, stdout = PIPE, stderr = PIPE)
+            f_output.write(reduce_proc.communicate(in_reduce.strip('\n'))[0])
+            in_reduce = key + '\t' + val
+    reduce_proc = Popen(["python", path_to_script], stdin = PIPE, stdout = PIPE, stderr = PIPE)
+    f_output.write(reduce_proc.communicate(in_reduce)[0])
+
+f_output.close()
--- InMemoryMapReduce/reduce.py
+++ InMemoryMapReduce/reduce.py
@@ -3,12 +3,5 @@ import sys
 word, count = sys.stdin.readline().split("\t")
 count = int(count)
 for line in sys.stdin:
-    cur_word, cur_count = line.split("\t")
-    cur_count = int(cur_count)
-    if cur_word == word:
-        count += cur_count
-    else:
-        print(word + "\t" + str(count))
-        word = cur_word
-        count = cur_count
-print (word + "\t" + str(count))
+    count += 1
+print(word + "\t" + str(count))
